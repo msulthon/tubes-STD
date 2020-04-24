77c115735b606fe733997f1d6ba4e33f4e270e9a
msulthon <59869908+msulthon@users.noreply.github.com>
2020-04-24 12:17:34 +0700

    Add files via upload

---
+++ tubes.cpp
@@ -0,0 +1,1 @@
+#include "tubes.h"
---
+++ tubes.h
@@ -0,0 +1,14 @@
+#ifndef TUBES_H_INCLUDED
+#define TUBES_H_INCLUDED
+#include <iostream>
+
+using namespace std;
+struct infotypeParent{
+    string warteg;
+    string IDwarteg
+    int Nowarteg;
+};
+typdef struct parent
+
+
+#endif // TUBES_H_INCLUDED
